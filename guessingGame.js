119e5ec541a69cd46a8a6b26150fecc0dc790ffc
Cory Baker <corybaker24@gmail.com>
2022-04-27 19:35:56 -0700

    secreNumber

--- guessingGame.js
+++ guessingGame.js
@@ -1,1 +1,19 @@
-'Hello'
+// const readline = require('readline')
+
+// const rl = readline.createInterface({
+//     input: process.stdin,
+//     outout:process.stdout
+// })
+
+
+let secretNumber = 1
+
+const checkGuess = num => {
+    if(num > secretNumber) return 'Too High ' + false
+    if(num < secretNumber) return 'Too Low ' + false
+    if(num === secretNumber) return 'Correct! ' + true
+}
+
+console.log(checkGuess(4))
+console.log(checkGuess(2))
+console.log(checkGuess(1))
